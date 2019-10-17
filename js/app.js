cab20e382bc4d2b258ede1d47a57f693b1a8b932
Elliot Dixon <elliotjohndixon@gmail.com>
2019-10-17 16:14:53 +0100

    finished

--- js/app.js
+++ js/app.js
@@ -4,15 +4,25 @@ document.addEventListener('DOMContentLoaded', () => {
   const form = document.querySelector("#new-item-form");
   form.addEventListener("submit", handleFormSubmit);
 
+  const deleteAllButton = document.querySelector('#delete-all');
+  deleteAllButton.addEventListener("click", deleteAllClick)
+
+
 })
 
-const readingList = []
 
 const handleFormSubmit = function (event) {
   event.preventDefault();
+  const newItem = document.createElement("p");
+  newItem.textContent = `${event.target.title.value} ${event.target.author.value} ${event.target.category.value}`;
+  newItem.classList.add("reading-list-item")
   const resultParagraph = document.querySelector("#reading-list");
-  resultParagraph.textContent = `${event.target.title.value} ${event.target.author.value} ${event.target.category.value}`;
-  readingList.push(newItem);
-  form.reset();
+  resultParagraph.appendChild(newItem);
+  this.reset();
+}
+
+const deleteAllClick = function (event) {
+  const resultParagraph = document.querySelector("#reading-list");
+  resultParagraph.textContent = null;
 
 }
